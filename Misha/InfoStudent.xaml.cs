b74428a9acfd50e6a852943df4237c85c9a3f46a
Bogovinka <77607705+Bogovinka@users.noreply.github.com>
2022-12-15 18:21:28 +0500

    Initial commit

---
+++ Misha/InfoStudent.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Misha
+{
+    /// <summary>
+    /// Логика взаимодействия для InfoStudent.xaml
+    /// </summary>
+    public partial class InfoStudent : Window
+    {
+        int idS;
+        DatabaseEntities db = new DatabaseEntities();
+        public InfoStudent(int id)
+        {
+            InitializeComponent();
+            idS = id;
+            dg.ItemsSource = db.Student.Where(x => x.ID == idS).ToList();
+            listDg.ItemsSource = db.Student_Task.Where(x => x.Student_id == idS).ToList();
+        }
+    }
+}
---
+++ Misha/Menu.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Misha
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        DatabaseEntities db = new DatabaseEntities();
+        public Menu()
+        {
+            InitializeComponent();
+            list.ItemsSource = db.Student.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            InfoStudent studentI = new InfoStudent(Convert.ToInt32(btn.Tag));
+            studentI.Show();
+        }
+    }
+}
